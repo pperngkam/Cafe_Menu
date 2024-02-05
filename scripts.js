e4221cdd3288f6bff75cf6b9dafa6395927d772d
pperngkam <144779836+pperngkam@users.noreply.github.com>
2024-02-05 09:10:33 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function showAlert() {
+    alert("Fun Fact: I love exploring new coffee flavors!");
+  }
+  
+  document
+    .querySelector(".nav-language-btn")
+    .addEventListener("click", showAlert);
+  
